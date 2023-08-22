910c0dfc37bb299d835e4dba138aadeefa1572c6
Khunakorn Johnson <khunakorn.j@datawow.io>
2023-08-22 20:25:06 +0700

    Add routing and fix validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   has_many :reviews
-  validates :name, precense: true
-  validates :release, precense: true
+  validates :name, presence: true
+  validates :release, presence: true
 end
