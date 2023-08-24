06fe20be5eccf7f0980e830dcc6c47606acf79d3
Khunakorn Johnson <khunakorn.j@datawow.io>
2023-08-24 11:54:18 +0700

    Add db constraints
    https://github.com/RobertDataWow/rails-book/pull/1#discussion_r1302487275

--- db/migrate/20230822090606_create_books.rb
+++ db/migrate/20230822090606_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :name
+      t.string :name, null: false, default: ''
       t.text :description
-      t.datetime :release
+      t.datetime :release, null: false
 
       t.timestamps
     end
