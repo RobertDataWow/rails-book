0f17f9fc4261a12fae48527012cca04ae1e3b41b
Khunakorn Johnson <khunakorn.j@datawow.io>
2023-08-25 13:11:50 +0700

    feat(Book/Review): ✨ Add User relation to book and review

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,9 +8,20 @@
 #  release     :datetime
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :bigint
+#
+# Indexes
+#
+#  index_books_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Book < ApplicationRecord
+  belongs_to :user
   has_many :reviews, dependent: :destroy
+
   validates :name, presence: true
   validates :release, presence: true
 
