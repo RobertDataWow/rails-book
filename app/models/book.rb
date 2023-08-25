76e6019756824c47b4ccad2844ec3d2ccd5a3fab
Khunakorn Johnson <khunakorn.j@datawow.io>
2023-08-25 14:05:10 +0700

    feat(Book): ✨ Add pagination
    Add 10 record per page pagination

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,9 +10,13 @@
 #  updated_at  :datetime         not null
 #
 class Book < ApplicationRecord
+  belongs_to :user
   has_many :reviews, dependent: :destroy
+
   validates :name, presence: true
   validates :release, presence: true
+
+  paginates_per 10
 
   def review_comments
     reviews.pluck(:comment)
