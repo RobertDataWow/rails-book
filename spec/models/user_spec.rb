2d5c52ae98922cfd77e8d8384f229c9a42f2c796
Khunakorn Johnson <khunakorn.j@datawow.io>
2023-08-29 16:41:38 +0700

    feat(GrapeApi): ✨ Setup model and gems

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  auth_token             :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  remember_created_at    :datetime
@@ -14,6 +15,7 @@
 #
 # Indexes
 #
+#  index_users_on_auth_token            (auth_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
